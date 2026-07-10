0e38d087519f53ba209c2cd1f1655403a6f77f0d
Matsvei Prakapovich <matveyprokopovech@gmail.com>
2026-07-10 14:35:19 +0300

    Implement task 3

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int result = 0;
+        while(number != 0) {
+            result = result * 10 + (number % 10);
+            number /= 10;
+        }
+        System.out.println(result);
     }
-
 }
