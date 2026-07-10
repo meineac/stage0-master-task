4739ac86b80be1be97e059b9c3bf057fd54e4904
Matsvei Prakapovich <matveyprokopovech@gmail.com>
2026-07-11 00:46:28 +0300

    Implement loops

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,16 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+        StringBuilder pattern = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; ++i) {
+            if (i == 1) {
+                pattern.append(1);
+            } else {
+                pattern.append(i).insert(0, i);
+            }
 
+            System.out.println(" ".repeat(cathetusLength - i) + pattern);
+        }
     }
 
     public static void main(String[] args) {
