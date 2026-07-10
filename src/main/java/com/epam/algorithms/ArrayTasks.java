99cba03a705166a6bc39da0f06a6cc8291ee1622
Matsvei Prakapovich <matveyprokopovech@gmail.com>
2026-07-11 00:47:04 +0300

    Implement algorithms

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        return new String[] {"winter", "spring", "summer", "autumn"};
     }
 
     /**
@@ -23,7 +23,13 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = i + 1;
+        }
+
+        return result;
     }
 
     /**
@@ -34,7 +40,11 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        return sum;
     }
 
     /**
@@ -46,7 +56,10 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] == number) return i;
+        }
+        return -1;
     }
 
     /**
@@ -58,7 +71,11 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        String[] res = new String[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+            res[i] = arr[arr.length - i - 1];
+        }
+        return res;
     }
 
     /**
@@ -70,7 +87,16 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        int k = 0;
+        for (var i : arr) {
+            if (i > 0) ++k;
+        }
+        int[] res = new int[k];
+        int pos = 0;
+        for (var i : arr) {
+            if (i > 0) res[pos++] = i;
+        }
+        return res;
     }
 
     /**
